8fc0539544641d6facba5bb89394db32fbcebb46
Delwin <delwin.lei@sjsu.edu>
2018-07-07 21:43:52 -0700

    updated a few functions

--- firmware/library/L1_Drivers/lpc_pwm.cpp
+++ firmware/library/L1_Drivers/lpc_pwm.cpp
@@ -1,8 +1,57 @@
 #include "lpc_pwm.h"
 
-PWM::PWM(bool vPort, uint8_t vChannel, uint32_t frequencyHz)
+//setMode(1, 5, 0b101);
+void setMode(uint8_t port, uint8_t pin, uint8_t mode)
+{
+  io_con_map->con[port][pin] = (io_con_map->con[port][pin] & (0b111)) | (mode & 0b111);
+}
+
+PWM::PWM(bool vPPort, uint8_t vPPin, uint32_t frequencyHz)
 {
 
+	//this saved value will be used to during destructor
+	pport = vPPort;	
+	ppin = vPPin;
+
+	//Enables PWM1 power/clock control bit
+	LPC_SC->PCONP |= (1 << 6);
+	//input clock is divided by 1
+	LPC_SC->PCLKSEL = (1 << 0);
+
+	matchValue = getSystemClock()/frequencyHz;
+
+	//Resets PWMTC on Match with MR0
+	LPC_PWM1->MCR |= (1 << 1);
+	LPC_PWM1->MR0 = matchValue;
+
+	//Enables PWM TC and PC for counting and enables PWM mode
+	LPC_PWM1->TCR = (1<< 0) | (1 << 3);
+	LPC_PWM1->CTCR &= ~(0xF << 0);
+
+	//sets port to proper function and get pwm channel
+	if(vPPort == 2)
+	{
+		setMode(vPPort, vPPin, 0b001);
+		port = vPPort - 1;
+		channel = vPPin + 1;		
+	}
+	else
+	{
+		setMode(vPPort, vPPin, 0b010);
+		if(vPPort == 3 && vPPin <= 21)
+		{
+			port = 0;
+			channel = vPPin - 15;
+		}
+		else if( vPPort == 3 && vPPin >= 24)
+		{
+			port = 1;
+			channel = vPPin - 23;
+		}
+	}
+
+	//Enables PWM[channel] output
+	LPC_PWM1->PCR |= (1 << (channel + 8));
 }
 
 PWM::~PWM()	//might need the private variable for double edge to turn off properly
@@ -15,28 +64,56 @@ PWM::~PWM()	//might need the private variable for double edge to turn off proper
 	{
 		LPC_PWM1->PCR &= ~(1 << (channel+8));
 	}
+	setMode(pport, ppin, 0b000);
 }
 
-void PWM::setDutyCycle(float percentage)
+void PWM::setDutyCycle(float percentage)	//will set the respective match register to [percentage]*MR0
+{
+	if(percentage < 0 || percentage > 100)
+	{
+		return;
+	}
+	uint32_t dutyValue = (percentage * matchValue) / 100;
+
+	switch(channel)
+	{
+		case 1: LPC_PWM1->MR1 = dutyValue;
+		case 2: LPC_PWM1->MR2 = dutyValue;
+		case 3: LPC_PWM1->MR3 = dutyValue;
+		case 4: LPC_PWM1->MR4 = dutyValue;
+		case 5: LPC_PWM1->MR5 = dutyValue;
+		case 6: LPC_PWM1->MR6 = dutyValue;
+		default: return;
+	}
+	LPC_PWM1->LER |= (1 << channel);
+}
+
+float PWM::getDutyCycle()		//will store in a variable? vs calculate it using match register
 {
 
 }
 
-float PWM::getDutyCycle()
+void PWM::setFrequency(uint32_t frequencyHz)	//this will set PWMEN = 0, set MR0, and set PWMEN = 1 
+{
+	//disables PWM mode; this will reset all counters to 0 and allow us to update MR0
+	LPC_PWM1->TCR &= ~(1 << 3);
+
+	matchValue = getSystemClock() / frequencyHz;
+	LPC_PWM1->MR0 = matchValue;
+
+	//re-enable PWM mode
+	LPC_PWM1->TCR |= (1 << 3);
+}
+
+uint32_t PWM::getFrequency()		//stored vs calculate
 {
 
 }
 
-void PWM::setFrequency(uint32_t frequencyHz)
-{
+/*
+	If PWM is set to double edge, it needs to turn on the next PWM channel by calling IOCON
 
-}
-
-uint32_t PWM::getFrequency()
-{
-
-}
-
+*/
 void PWM::setAsDoubleEdge(bool vIsDoubleEdge = true)
 {
 	isDoubleEdge = vIsDoubleEdge;
--- firmware/library/L1_Drivers/lpc_pwm.h
+++ firmware/library/L1_Drivers/lpc_pwm.h
@@ -1,12 +1,25 @@
 #ifndef LPC_PWM_H_
 #define LPC_PWM_H_
 
+#include <cstdint>
+#include <cstdio>
 #include "LPC40xx.h"
+
+typedef uint32_t io_con_register;
+
+LPC_IOCON_TypeDef IO_CON_LOCAL = {0};
+
+struct io_con_map_t 
+{
+    io_con_register con[5][32];
+};
+
+io_con_map_t * io_con_map = reinterpret_cast<io_con_map_t *>(&IO_CON_LOCAL);
 
 class PWM
 {
 public:
-	PWM(bool vPort, uint8_t vChannel, uint32_t frequencyHz);
+	PWM(bool vPPort, uint8_t vPPin, uint32_t frequencyHz = 50);
 	~PWM();
 	void setDutyCycle(float percentage);
 	float getDutyCycle();
@@ -16,9 +29,16 @@ public:
 	bool getEdge();
 
 private:
+	static uint32_t matchValue;
 	bool isDoubleEdge;
+	
+	//will signify current PWM0/1 and channel
 	bool port;
 	uint8_t channel;
+
+	//these will be used to identify which pin on the board is used and will activate that pin to pwm
+	uint8_t pport;		//port number of pin
+	uint8_t ppin;		//pin number
 };
 
 #endif LPC_PWM_H_
